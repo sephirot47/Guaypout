38a140840cc436b201bc0af34ef4f3224b6a4078
Oscar Mañas <oscar.manas@est.fib.upc.edu>
2016-11-14 17:57:17 +0100

    Uploaded files

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public GameObject player;
+	private float distance;
+
+	void Start () {
+		distance = Vector3.Distance(transform.position, player.transform.position);
+	}
+
+	void Update () {
+		Vector3 offsetDir = Vector3.Normalize(-player.transform.forward + player.transform.up * 0.5f);
+		transform.position = player.transform.position + offsetDir * distance;
+		transform.LookAt (player.transform);
+	}
+}
