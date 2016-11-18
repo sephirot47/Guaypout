c7ee9148f0d9f591e0293766959413e3e9b4cc8b
Oscar Mañas <oscar.manas@est.fib.upc.edu>
2016-11-18 13:28:47 +0100

    Improved movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour {
 	public float hoverForce = 9f;
 	public float hoverHeight = 2f;
 	public GameObject[] hoverPoints;
+	public TrackInformer trackInformer;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
@@ -40,12 +41,12 @@ public class PlayerController : MonoBehaviour {
 		RaycastHit hit;
 		for (int i = 0; i < hoverPoints.Length; ++i) {
 			GameObject hoverPoint = hoverPoints[i];
-			Ray ray = new Ray(hoverPoint.transform.position, -Vector3.up);
-			Debug.DrawRay (ray.origin, ray.direction, Color.red, 0.05f);
+			Ray ray = new Ray(hoverPoint.transform.position, -transform.up);
+			Debug.DrawRay (ray.origin, ray.direction, Color.red, 0f);
 			if (Physics.Raycast (ray, out hit, hoverHeight, layerMask)) {
 				float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
-				rb.AddForceAtPosition (Vector3.up * hoverForce * proportionalHeight, hoverPoint.transform.position);
-				Debug.DrawRay (ray.origin, ray.direction, Color.green, 0.05f);
+				rb.AddForceAtPosition (transform.up * hoverForce * proportionalHeight, hoverPoint.transform.position);
+				Debug.DrawRay (ray.origin, ray.direction, Color.green, 0f);
 			}
 		}
 
@@ -56,5 +57,11 @@ public class PlayerController : MonoBehaviour {
 		// Turn
 		if (turn != 0)
 			rb.AddRelativeTorque (transform.up * turn * turnStrength);
+
+		// Lock the forward vector
+		TrackInformer.TrackInfo trackInfo = trackInformer.GetTrackInfo (transform.position, transform.right, transform.up, hoverHeight * 2);
+		Quaternion endRotation = Quaternion.LookRotation (trackInfo.forward, trackInfo.normal);
+		//transform.rotation = endRotation;
+		transform.rotation = Quaternion.Slerp (transform.rotation, endRotation, 5*Time.deltaTime);
 	}
 }
