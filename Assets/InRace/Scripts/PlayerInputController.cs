b72bbab090e8bb1908538b747028382459cdf23a
sephirot47 <victorantondominguez@gmail.com>
2016-12-05 16:59:31 +0100

    Hit animation and control lose done

---
+++ Assets/InRace/Scripts/PlayerInputController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputController : ShipInputController 
+{
+    void Start()
+    {
+        base.Start();
+    }
+
+	void Update()
+    {
+        base.Update();
+        if (currentState == ShipInputController.State.Moving)
+        { 
+		    float verticalAxis = Input.GetAxis ("Vertical");
+		    float horizontalAxis = Input.GetAxis ("Horizontal");
+		    shipPhysicsController.SetThrust (verticalAxis);
+		    shipPhysicsController.SetTurn (horizontalAxis);
+        }
+        else if (currentState == ShipInputController.State.Hit)
+        {
+        }
+    }
+
+    public void OnGoalPassed()
+    {
+        GetComponentInChildren<Animator>().SetTrigger("Boosting");
+        this.enabled = false;
+    }
+}
