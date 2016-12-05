c7ec0313a2cb1798d7f8dee90aa2fbf5d6bce9dd
oscmansan <oscmansan@gmail.com>
2016-12-05 17:51:45 +0100

    Started projectiles

---
+++ Assets/InRace/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    [HideInInspector]
+    public GameObject player;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponentInChildren<Rigidbody>();
+        rb.velocity = player.transform.forward * 20f;
+
+        Destroy(gameObject, 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
