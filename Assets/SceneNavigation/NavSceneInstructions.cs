b92149df25f688460b0296547e25443162b7fdc9
sephirot47 <victorantondominguez@gmail.com>
2016-12-06 22:13:12 +0100

    Navigation to credits and instructions back and forth done

--- Assets/SceneNavigation/NavSceneInstructions.cs
+++ Assets/SceneNavigation/NavSceneInstructions.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class NavSceneInstructions : INavScene
 {
+    private GameObject canvas;
+    new void Awake()
+    {
+        base.Awake();
+        canvas = transform.FindChild("Canvas").gameObject;
+    }
+
     override public void Activate ()
     {
-
+        canvas.SetActive(true);
     }
 
     override public void Deactivate ()
     {
-
+        canvas.SetActive(false);
     }
 }
