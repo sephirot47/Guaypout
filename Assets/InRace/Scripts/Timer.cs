53d9aef22910b75069c4315c96817a956d451992
Oscar Mañas <oscar.manas@est.fib.upc.edu>
2016-12-06 21:18:18 +0100

    Added power-ups to orbes and refill times

---
+++ Assets/InRace/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	private float timeLeft;
+
+	void Start () {
+		timeLeft = 0f;
+	}
+
+	void Update () {
+		timeLeft -= Time.deltaTime;
+	}
+
+	public void Set(float t) {
+		timeLeft = t;
+	}
+
+	public bool Ended() {
+		return timeLeft <= 0;
+	}
+}
