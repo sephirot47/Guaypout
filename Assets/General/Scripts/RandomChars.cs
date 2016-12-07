5bba4ca4eda316524b33b951f49e886543b4456f
sephirot47 <victorantondominguez@gmail.com>
2016-12-08 00:44:41 +0100

    Added random puke text as in movies of super hackers

---
+++ Assets/General/Scripts/RandomChars.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RandomChars : MonoBehaviour 
+{
+    public string possibleChars = "abcdefghijklmnopqrstuvwxyz1234567890#-.";
+    public float changeSpeed;
+    public int minDigits, maxDigits;
+
+    private Text text;
+    private float changeChrono = 0.0f;
+	void Start () 
+    {
+        changeSpeed *= Random.Range(0.8f, 1.2f);
+
+        text = GetComponent<Text>();
+        FillTextWithRandomPuke();
+	}
+	
+	void Update () 
+    {
+        changeChrono += Time.deltaTime;
+        if (changeChrono >= changeSpeed)
+        {
+            changeChrono = 0.0f;
+            FillTextWithRandomPuke();
+        }
+	}
+
+    void FillTextWithRandomPuke()
+    {
+        int digits = Random.Range(minDigits, maxDigits + 1);
+        string newRandomString = "";
+        for (int i = 0; i < digits; ++i)
+        {
+            newRandomString += possibleChars[Random.Range(0,possibleChars.Length)];
+        }
+
+        text.text = newRandomString;
+    }
+}
