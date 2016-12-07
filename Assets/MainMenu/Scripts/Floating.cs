aaf5e2993b29e6eedb817e50d208c437cf9b644c
sephirot47 <victorantondominguez@gmail.com>
2016-12-07 23:58:21 +0100

    WIP. HUD revamp

---
+++ Assets/MainMenu/Scripts/Floating.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floating : MonoBehaviour 
+{
+    public float floatAmount;
+    public float floatVelocity;
+
+    private float time;
+	void Start () 
+    {
+        time = Random.Range(0.0f, 3.14592f);
+        floatAmount *= Random.Range(0.8f, 1.2f);
+        floatVelocity *= Random.Range(0.8f, 1.2f);
+	}
+	
+	void Update ()
+    {
+        time += floatVelocity * Time.deltaTime;
+        transform.position += Vector3.up * (Mathf.Sin(time) * floatAmount);
+	}
+}
