5245f4ca5b8ed44e3876726bc53668f2a245febf
sephirot47 <victorantondominguez@gmail.com>
2016-12-09 12:56:53 +0100

    Instructions done

--- Assets/SceneNavigation/NavSceneInstructions.cs
+++ Assets/SceneNavigation/NavSceneInstructions.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class NavSceneInstructions : INavScene
 {
-    private GameObject canvas;
+    private GameObject canvas, worldCanvas;
     new void Awake()
     {
         base.Awake();
         canvas = transform.FindChild("Canvas").gameObject;
+        worldCanvas = transform.FindChild("WorldCanvas").gameObject;
     }
 
     override public void Activate ()
     {
         canvas.SetActive(true);
+        worldCanvas.SetActive(true);
     }
 
     override public void Deactivate ()
     {
         canvas.SetActive(false);
+        worldCanvas.SetActive(false);
     }
 }
