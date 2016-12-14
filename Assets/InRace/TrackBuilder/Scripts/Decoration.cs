3a2fe60ee6158b233ae641ea22b3af559412963d
sephirot47 <victorantondominguez@gmail.com>
2016-12-14 19:42:01 +0100

    Added random decorations to circuit

---
+++ Assets/InRace/TrackBuilder/Scripts/Decoration.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Decoration : MonoBehaviour 
+{
+    public float appearingProbs;
+	void Start ()
+    {
+        if (Random.Range(0.0f, 1.0f) > appearingProbs)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	void Update () 
+    {
+	
+	}
+}
