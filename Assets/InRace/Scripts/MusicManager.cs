0f831866eeb9e463c02752ecc7b42e78fd2b6cf6
Oscar Mañas <oscar.manas@est.fib.upc.edu>
2016-12-16 00:38:49 +0100

    Added MusicManager

---
+++ Assets/InRace/Scripts/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] songs;
+
+	private AudioSource source;
+
+	void Start () {
+		source = GetComponentInParent<AudioSource> ();
+		PlayRandomSong ();
+	}
+
+	void Update() {
+		if (!source.isPlaying) {
+			PlayRandomSong ();
+		}
+	}
+
+	private void PlayRandomSong() {
+		int n = songs.Length;
+		int i = Random.Range (0, n);
+		source.clip = songs [i];
+		source.Play ();
+	}
+}
